eacd5cd65876f9d7d97445bb98be9244bdec24bf
Kazuya Ueoka <info@fromkk.info>
2020-01-18 14:46:00 +0900

    Initial Commit

---
+++ CatalystSampler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CatalystSampler
+//
+//  Created by Kazuya Ueoka on 2020/01/18.
+//  Copyright © 2020 fromKK. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
